7c1ce4de1e4bca25ca78f5b037321fb00e88456d
Tianyu Yu <t43yu@uwaterloo.ca>
2016-06-11 22:19:56 -0400

    Updated logic WIP

--- prototype/src/java/src/rsync/client/uploader/RsyncAnalyser.java
+++ prototype/src/java/src/rsync/client/uploader/RsyncAnalyser.java
@@ -22,14 +22,15 @@ public class RsyncAnalyser {
     }
 
     /**
-     * Given two set of hashes for a supposedly possibly incomplete remote version of a file (specified by update(BufferedInputStream)),
-     * generate instructions for the remote machine to recreate the file.
+     * Given two set of hashes for a supposedly possibly incomplete remote version of a file
+     * (specified by update(BufferedInputStream)), generate instructions for the remote machine to recreate the file.
      *
      * @param remoteRollingChksms      a List of Adler-32 checksums calculated for the remote file
      * @param remoteMD5Chksms          a List of MD5 checksums calculated for the remote file
      * @param blockSizes               the size of the data block for each checksum, in corresponding order
      */
     public Object generate(List<Long> remoteRollingChksms, List<String> remoteMD5Chksms, List<Integer> blockSizes) {
+        // TODO: Have a list of tuples for the two checksums instead of two lists.
         // TODO: Differentiate between different types of errors
         // TODO: Is it better to simplify blockSizes such that it's only a tuple of (defaultSize: lastBlockSize)?
         if (this.dataStream == null) {
@@ -42,20 +43,22 @@ public class RsyncAnalyser {
             return null;
         }
 
-        List<Long> localRollingChksms;
-        try {
-            localRollingChksms = this.preprocessLocalRollingChecksums(blockSizes.get(0));
-        }
-        catch (IOException e) {
-            // TODO: Error code here for cannot access local file
-            return null;
-        }
+        // Preprocess remote rolling checksums for faster lookup
+        HashMap<Long, Integer> remoteRollingMap = this.preprocessRemoteRollingChecksums(remoteRollingChksms);
 
-        // Do a linear scan on remote rolling checksum. Record the spaces skipped
+        // For each local checksum (at every offset), check to see if it is present in the map.
+        // If it is, check MD5. Skip by $(defaultBlockSize) and write its index to the instructions
+            // For everything between it and its previous ending, write those bytes to the instructions
+            // If rolling matches but MD5 doesn't, we assume it's a coincidence and move on?
+        // If it's not, go to next byte and repeat.
 
-        // MD5 validate each block
-
-        // Process results
+        // When we finish all blocks of size $(defaultBlockSize), do we look at blocks of lesser sizes, since
+        // it's possible that there's a dangling block on remote file?
+        // Self answer: No. If our local checking ends with less than a block left, we simply
+        // send all those bytes over?
+        // Could be made more efficient, but not very likely that there's a dangling block at the end...
+            // Once the end is reached, or there's not enough for a block, we can use incrementally smaller sizes
+            // starting from the end and repeat until a block size of 0 is reached.
     }
 
     private boolean checkBlockSizes(List<Integer> blockSizes) {
@@ -68,18 +71,27 @@ public class RsyncAnalyser {
     }
 
     /**
-     * Preprocess the local file for transmission by calculating its rolling checksum at every offset
-     * for a given block size.
+     * Given a list of checksums, convert it into a HashMap of its checksum values to its indices.
      *
-     * @param blockSize         size of each data block in bytes
-     * @return                  a List of Adler32 rolling checksums
-     * @throws IOException
+     * @param remoteRollingChksms       a List of Adler-32 rolling checksums
+     * @return                          a mapping of checksums to its index
      */
-    private List<Long> preprocessLocalRollingChecksums(int blockSize) throws IOException {
+    private HashMap<Long, Integer> preprocessRemoteRollingChecksums(List<Long> remoteRollingChksms) {
+        HashMap<Long, Integer> ret = new HashMap<>();
+        for (int i = 0; i < remoteRollingChksms.size(); i++) {
+            ret.put(remoteRollingChksms.get(i), i);
+        }
+        return ret;
+    }
+
+    /*
+    THIS IS WRONG. KEEPING SO THAT I CAN REUSE CODE LATER.
+
+    private HashMap<Long, Integer> preprocessLocalRollingChecksums(int blockSize) throws IOException {
         // Public methods should check this before calling helper
         assert dataStream != null;
 
-        List<Long> ret = new ArrayList<>();
+        HashMap<Long, Integer> ret = new HashMap<>();
         byte[] firstBlock = new byte[blockSize];
         byte[] b = new byte[1];
 
@@ -89,16 +101,18 @@ public class RsyncAnalyser {
             return null;
         }
 
+        int i = 0;
         byte previousFirstByte = firstBlock[0];
         long rollingChecksum = adler.calc(firstBlock, blockSize);
-        ret.add(rollingChecksum);
+        ret.put(rollingChecksum, i++);
         while (dataStream.read(b) == 1) {
             rollingChecksum = adler.calc(rollingChecksum, blockSize, previousFirstByte, b[0]);
             previousFirstByte = b[0];
-            ret.add(rollingChecksum);
+            ret.put(rollingChecksum, i++);
         }
         return ret;
     }
+    */
 
     /**
      * Given a block of data, return a String representation of its MD5 checksum.
