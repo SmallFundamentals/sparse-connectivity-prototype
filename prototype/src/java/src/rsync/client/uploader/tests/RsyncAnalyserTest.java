b5d8ed3eba938735127a163dd0cea18407004343
Tianyu Yu <t43yu@uwaterloo.ca>
2016-06-12 22:29:35 -0400

    Unit test file for RsyncAnalyser

---
+++ prototype/src/java/src/rsync/client/uploader/tests/RsyncAnalyserTest.java
@@ -0,0 +1,47 @@
+package rsync.client.uploader.tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import rsync.client.uploader.RsyncAnalyser;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class RsyncAnalyserTest {
+
+    private static String TEST_FILE_PATH = "./src/rsync/client/uploader/tests/assets/sm_img.jpeg";
+
+    RsyncAnalyser analyser;
+    BufferedInputStream dataStream;
+
+    @Before
+    public void setUp() throws Exception {
+        this.analyser = new RsyncAnalyser();
+        this.dataStream = getBufferedIStream();
+    }
+
+    @Test
+    public void testGenerate() throws Exception {
+        this.analyser.update(this.dataStream);
+        System.out.println(this.analyser.generate(new ArrayList<>(), new ArrayList<>(), 1024, 1024));
+    }
+
+    @Test
+    public void testPreprocessRemoteRollingChecksums() {
+
+    }
+
+    @Test
+    public void testGetMD5HashString() {
+
+    }
+
+    private static BufferedInputStream getBufferedIStream() throws FileNotFoundException {
+        BufferedInputStream dataStream = new BufferedInputStream(new FileInputStream(TEST_FILE_PATH));
+        return dataStream;
+    }
+}
