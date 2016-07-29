cd5eec335284e3d2d29097656c52adc7947c1fb8
Tianyu Yu <t43yu@uwaterloo.ca>
2016-07-29 18:40:34 -0400

    Partial test -- currently doesn't work, exposed a bug

--- prototype/src/java/src/rsync/client/uploader/tests/RsyncAnalyserTest.java
+++ prototype/src/java/src/rsync/client/uploader/tests/RsyncAnalyserTest.java
@@ -14,6 +14,8 @@ public class RsyncAnalyserTest {
     private static String TEST_FILE_PATH = "./src/rsync/client/uploader/tests/assets/sm_img.jpeg";
     private static String ROLLING_CHECKSUM_FILENAME = "./src/rsync/client/uploader/tests/assets/sm_img_rolling.sum";
     private static String MD5_CHECKSUM_FILENAME = "./src/rsync/client/uploader/tests/assets/sm_img_md5.sum";
+    private static String PARTIAL_0_ROLLING_CHECKSUM_FILENAME = "./src/rsync/client/uploader/tests/assets/partial_0_rolling.sum";
+    private static String PARTIAL_0_MD5_CHECKSUM_FILENAME = "./src/rsync/client/uploader/tests/assets/partial_0_md5.sum";
 
     RsyncAnalyser analyser;
     BufferedInputStream dataStream;
@@ -42,6 +44,15 @@ public class RsyncAnalyserTest {
         for (int i = 0; i < 19; i++) {
             assertEquals(i, instructions.get(i));
         }
+    }
+
+    @Test
+    public void testGeneratePartialFile() throws Exception {
+        this.analyser.update(this.dataStream);
+        List<Long> rolling = this.getRollingChecksumList(PARTIAL_0_ROLLING_CHECKSUM_FILENAME);
+        List<String> md5 = this.getMD5ChecksumList(PARTIAL_0_MD5_CHECKSUM_FILENAME);
+        List<Object> instructions = this.analyser.generate(rolling, md5, 1024, 1024);
+        System.out.println(instructions);
     }
 
     private static BufferedInputStream getBufferedIStream() throws FileNotFoundException {
