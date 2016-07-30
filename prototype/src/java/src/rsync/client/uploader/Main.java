9a71bbb3ae9c14d87891a6acba93cb880b174ce6
Tianyu Yu <t43yu@uwaterloo.ca>
2016-07-30 18:15:18 -0400

    Write instructions to file in Main

--- prototype/src/java/src/rsync/client/uploader/Main.java
+++ prototype/src/java/src/rsync/client/uploader/Main.java
@@ -1,7 +1,10 @@
 package rsync.client.uploader;
 
 import java.io.*;
+import java.nio.charset.*;
+import java.nio.file.*;
 import java.security.*;
+import java.util.*;
 
 public class Main {
 
@@ -20,9 +23,84 @@ public class Main {
         }
 
         RsyncAnalyser analyser = new RsyncAnalyser();
+        analyser.update(dataStream);
+
+        String PARTIAL_0_ROLLING_CHECKSUM_FILENAME = "./src/rsync/client/uploader/tests/assets/partial_0_rolling.sum";
+        String PARTIAL_0_MD5_CHECKSUM_FILENAME = "./src/rsync/client/uploader/tests/assets/partial_0_md5.sum";
+        List<Long> rolling = getRollingChecksumList(PARTIAL_0_ROLLING_CHECKSUM_FILENAME);
+        List<String> md5 = getMD5ChecksumList(PARTIAL_0_MD5_CHECKSUM_FILENAME);
+        List<Object> instructions = analyser.generate(rolling, md5, 1024, 1024);
+        pseudosend(instructions);
     }
 
     private static void send() {
 
+    }
+
+    /**
+     * Writes to a file to be parsed by python client, instead of sending it through network protocols
+     * @param instructions
+     */
+    private static void pseudosend(List<Object> instructions) throws IOException {
+        Path file = Paths.get("instr.out");
+        // Write an empty byte to clear the file
+        Files.write(file, new byte[0]);
+        try {
+            for (int i = 0; i < instructions.size(); i++) {
+                if (instructions.get(i) instanceof ArrayList) {
+                    // Raw byte data
+                    List<Byte> data = (List<Byte>) instructions.get(i);
+                    Byte[] bytes = data.toArray(new Byte[data.size()]);
+                    Files.write(file, getRawBytes(bytes), StandardOpenOption.APPEND);
+                } else if (instructions.get(i) instanceof Integer) {
+                    // Int
+                    List<String> lines = Arrays.asList(Integer.toString((int) instructions.get(i)));
+                    Files.write(file, lines, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+                }
+            }
+            List<String> lines = Arrays.asList("\n");
+            Files.write(file, lines, Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.out.println(e);
+            throw e;
+        }
+    }
+
+    // TODO: Put this in a util file so it can be reused
+    private static List<Long> getRollingChecksumList(String path) throws IOException {
+        List<Long> rollingChecksums = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(path)))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                long b = Long.parseLong(line);
+                rollingChecksums.add(b);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+            throw e;
+        }
+        return rollingChecksums;
+    }
+
+    private static List<String> getMD5ChecksumList(String path) throws IOException {
+        List<String> checksum = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(path)))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                checksum.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+            throw e;
+        }
+        return checksum;
+    }
+
+    private static byte[] getRawBytes(Byte[] data) {
+        byte[] raw = new byte[data.length];
+        for (int i = 0; i < data.length; i++) {
+            raw[i] = data[i];
+        }
+        return raw;
     }
 }
