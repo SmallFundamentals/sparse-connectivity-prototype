1e07b4f0584368f3cba741cf81c28fcb04c9f444
Blake Yu <smallfundamentals@gmail.com>
2017-01-03 16:03:31 -0500

    Fix hard-coded original file path

--- prototype/src/java/src/main/java/rsync/client/uploader/Main.java
+++ prototype/src/java/src/main/java/rsync/client/uploader/Main.java
@@ -13,30 +13,31 @@ public class Main {
     public final static String ORIGINAL_FILENAME = "../../assets/sm_img.jpeg";
 
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException, DigestException {
-        for (String partialFileName: args) {
-            System.out.println(String.format("UPLOADING for test - '%s'", partialFileName));
-            System.out.println();
+        String partialFileName = args[0];
+        String originalFilePath = args[1];
 
-            ChecksumResult cr = NetworkUtil.getChecksumResult(ORIGINAL_FILENAME, partialFileName);
+        System.out.println(String.format("UPLOADING for test - '%s'", partialFileName));
+        System.out.println();
 
-            BufferedInputStream dataStream;
-            try {
-                dataStream = new BufferedInputStream(new FileInputStream(ORIGINAL_FILENAME));
-            } catch (FileNotFoundException e) {
-                System.out.println(e);
-                return;
-            }
+        ChecksumResult cr = NetworkUtil.getChecksumResult(originalFilePath, partialFileName);
 
-            RsyncAnalyser analyser = new RsyncAnalyser();
-            analyser.update(dataStream);
-
-            List<Long> rolling = cr.getRolling();
-            List<String> md5 = cr.getMd5();
-            List<Object> instructions = analyser.generate(rolling, md5, 1024, 1024);
-
-            NetworkUtil.send(partialFileName, instructions);
-
-            System.out.println();
+        BufferedInputStream dataStream;
+        try {
+            dataStream = new BufferedInputStream(new FileInputStream(originalFilePath));
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            return;
         }
+
+        RsyncAnalyser analyser = new RsyncAnalyser();
+        analyser.update(dataStream);
+
+        List<Long> rolling = cr.getRolling();
+        List<String> md5 = cr.getMd5();
+        List<Object> instructions = analyser.generate(rolling, md5, 1024, 1024);
+
+        NetworkUtil.send(partialFileName, instructions);
+
+        System.out.println();
     }
 }
